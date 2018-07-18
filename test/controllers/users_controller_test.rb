27248bc97d63ee8e4c1b7a53ab9e24cb2a3b06ea
Nora Peters <norapeters5@gmail.com>
2018-07-17 19:33:17 -0700

    test writing/stubbing in process

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,23 @@
 require "test_helper"
+require 'pry'
 
 describe UsersController do
   describe '#create' do
     # what is my error return format?
-    it 'should not signup without password'
+    it 'should not signup without password' do
+      post users_url,
+        params: {
+          user: {
+            email: "test@test.com",
+          }
+        }
+
+      user = JSON.parse(response.body)
+      # assert_equal(user["errors"]["password"]["can't be blank"])
+      # user.must_raise("can't be blank")
+      # binding.pry
+    end
+
     it 'should not signup without email'
 
     it 'should return the User object if successful' do
@@ -22,5 +36,10 @@ describe UsersController do
       assert_equal("test@test.com", user['email'])
       assert(!user.has_key?('password'))
     end
+  end
+
+  describe '#signin' do
+    it 'should allow existing user to sign in'
+    it 'should throw error when non-existant user attempts to sign in'
   end
 end
