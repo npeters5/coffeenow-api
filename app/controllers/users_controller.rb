025bbc70faeb2dd4492abceb296c1b7839e88051
Nora Peters <norapeters5@gmail.com>
2018-07-18 15:32:30 -0700

    modifies user#create to look for existing user before creating new. user#signin looks for user by email then password and returns custom error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
+require 'pry'
+
 class UsersController < ApplicationController
   def create
     user = User.new(allowed_params)
-    if user.save
+    if User.exists?(email: allowed_params["email"])
+      render json: { ok: false, errors: "User already exists" },
+      status: :bad_request
+    elsif user.save
       render json: user
     else
       render json: { ok: false, errors: user.errors },
@@ -10,12 +15,17 @@ class UsersController < ApplicationController
   end
 
   def sign_in
-    user = User.find_by(email: email, password: password)
-
+    user = User.find_by(email: allowed_params["email"])
     if user.present?
-      # yay
+      if user.password == allowed_params["password"]
+        render json: user
+      else
+        render json: { ok: false, errors: "Password incorrect" },
+        status: :unauthorized
+      end
     else
-      # error
+      render json: { ok: false, errors: "Please sign up" },
+      status: :bad_request
     end
   end
 
