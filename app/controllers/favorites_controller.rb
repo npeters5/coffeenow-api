37987ddfbe33bd0514e0eacda401edf60b5f3a4d
Nora Peters <norapeters5@gmail.com>
2018-07-21 10:28:27 -0700

    implements favorite#create with test post request successful

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,8 +7,30 @@ class FavoritesController < ApplicationController
   end
 
   def create
+    user = find_current_user_from_request_header
+    fav = Favorite.new(shop_id: params[:shop_id], user_id: user.id)
+    if fav.save
+      render json: fav
+    else
+      render json: { ok: false, errors: fav.errors },
+      status: :bad_request
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def find_current_user_from_request_header
+    User.find_by(api_token: api_token)
+  end
+
+  def api_token
+    request.headers['X-Api-Token']
+  end
+
+  def fav_params
+    params.require(:favorite).permit(:shop_id, :user_id)
   end
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,3 +1,6 @@
 class Favorite < ApplicationRecord
   belongs_to :user
+
+  validates :shop_id, presence: true
+  validates :user_id, presence: true
 end
