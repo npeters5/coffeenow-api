118920f23f5db70bacfead1e2fda41ad7fe14875
Nora Peters <norapeters5@gmail.com>
2018-07-22 14:20:50 -0700

    adds validation to fav model to check for unique shop id per user. adds favorited true/false check to shops#index and shops#show

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,14 +1,22 @@
 class ShopsController < ApplicationController
+  before_action :require_user!, only: [:index, :show]
+
   def index
     latitude = params[:latitude]
     longitude = params[:longitude]
     shops = YelpApiWrapper.list_coffeeshops(latitude, longitude)
+    shops.each do |shop|
+      # is this working?
+      # for your initial load of [..., ..., ...]
+      shop.favorited! if current_user.favorited?(shop)
+    end
     render json: shops.as_json
   end
 
   def show
     shop_id = params[:id]
     shop = YelpApiWrapper.get_shop_detail(shop_id)
+    shop.favorited! if current_user.favorited?(shop)
     render json: shop.as_json
   end
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,6 @@
 class Favorite < ApplicationRecord
   belongs_to :user
 
-  validates :shop_id, presence: true, uniqueness: true
+  validates :shop_id, presence: true, uniqueness: { scope: :user_id }
   validates :user_id, presence: true
 end
