9d3ecf7549e6d2e6d24b8260facefd58310c405a
Nora Peters <norapeters5@gmail.com>
2018-07-22 14:22:13 -0700

    adds optional args to Shop initialize

--- lib/Shop.rb
+++ lib/Shop.rb
@@ -2,7 +2,7 @@ class Shop
 
   attr_reader :name, :id, :image_url, :url, :rating, :latitude, :longitude
 
-  def initialize(name, id, image_url, url, rating, latitude, longitude )
+  def initialize(name, id, image_url, url, rating, latitude, longitude, **args)
     raise ArgumentError if name.nil? || name == "" || id.nil? || id == ""
     @name = name
     @id = id
@@ -10,6 +10,16 @@ class Shop
     @url = url
     @rating = rating
     @coords = {latitude: latitude, longitude: longitude}
+    @display_phone = args[:display_phone] ||= nil
+    @review_count = args[:review_count]
+    @photos = args[:photos]
+    @address = args[:address]
+    @hours = args[:hours]
+    @favorited = false
+  end
+
+  def favorited!
+    @favorited = true
   end
 
 end
