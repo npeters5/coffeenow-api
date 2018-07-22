3575d0a1911eb4022df008f8cb4d5a73eb87d95f
Nora Peters <norapeters5@gmail.com>
2018-07-22 14:22:50 -0700

    refactors get_shop_detail wrapper method to pass optional args to Shop.new

--- lib/yelp_api_wrapper.rb
+++ lib/yelp_api_wrapper.rb
@@ -22,8 +22,21 @@ class YelpApiWrapper
   def self.get_shop_detail(shop_id)
     headers = {'Authorization' => "Bearer #{TOKEN}" }
     response = HTTParty.get("#{URL}/#{shop_id}", headers: headers )
+    shop = {}
     if response
-      return response
+      shop = Shop.new(response["name"], response["id"], response["image_url"], response["url"], response["rating"], response["coordinates"]["latitude"], response["coordinates"]["longitude"],
+      {
+        display_phone: response["display_phone"],
+        review_count: response["review_count"],
+        photos: response["photos"],
+        address: response["location"]["display_address"],
+        hours: response["hours"]
+      })
+      # return JSON.parse(response.body)
+      #  response = JSON.parse(response.body, object_class: OpenStruct)
+      #  return response
+      # return shop
     end
+    return shop
   end
 end
